4d1d7551a09f9a8592ed1090c46b5520b610ae98
devjeel <jeelhp02@gmail.com>
2019-02-19 13:50:48 -0500

    error fix

--- Heart_Disease_Model.py
+++ Heart_Disease_Model.py
@@ -1,56 +1,56 @@
-#Importing libraries 
+# Importing libraries
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-#Load Dataset
+# Load Dataset
 dataset = pd.read_csv('heart.csv')
 
-#Selecting Features 
+# Selecting Features
 X = dataset.iloc[:, :-1]
 
-#Selecting Target 
+# Selecting Target
 y = dataset.iloc[:, -1]
 
-#Printing Features And Target names
-#print('Features :' , X)
-#print('Target :', y)
+# Printing Features And Target names
+# print('Features :' , X)
+# print('Target :', y)
 
-#Printing Shapes
+# Printing Shapes
 print(X.shape)
 print(y.shape)
 
-#Splitting Trainning and testing Data
+# Splitting Training and testing Data
 from sklearn.model_selection import train_test_split
-train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=0) 
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=0)
 
-#KNeighborsClassifier Trainning Model
+# KNeighborsClassifier Training Model
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(train_X, train_y)
 
-#Predicting value from test set
+# Predicting value from test set
 test_prediction = knn.predict(test_X)
 
-#Accuracy Score
+# Accuracy Score
 from sklearn import metrics
-print(metrics.accuracy_score(test_y, test_prediction))  #OUTPUT: 0.5494505494505495
+print(metrics.accuracy_score(test_y, test_prediction))  # OUTPUT: 0.5494505494505495
 
-#Ploting best K value for KNN
+# Plotting best K value for KNN
 v = []
 k_range = list(range(1,50))
 for i in k_range:
     knn = KNeighborsClassifier(n_neighbors=i)
-    #fit the model with trainning data
+    # fit the model with training data
     knn.fit(train_X, train_y)
     pred = knn.predict(test_X)
-    #adding all accuracy result to list
+    # adding all accuracy result to list
     v.append(metrics.accuracy_score(test_y, pred))
 
 plt.plot(k_range, v, c='orange')
 plt.show()
 
-#trainning model with best K value
+# Training model with best K value
 knn = KNeighborsClassifier(n_neighbors=18)
 knn.fit(train_X, train_y)
 test_prediction = knn.predict(test_X)
-print(metrics.accuracy_score(test_y, test_prediction)) #OUTPUT: 0.7472527472527473
+print(metrics.accuracy_score(test_y, test_prediction))  # OUTPUT: 0.7472527472527473
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Heart_Disease_KNN_Model
 
-Data source: <a herf="https://www.kaggle.com/ronitf/heart-disease-uci" title="Kaggle Link">Kaggle Dataset</a>
+Data source: <a herf='https://www.kaggle.com/ronitf/heart-disease-uci' title='Kaggle Link'>Kaggle Dataset</a>
 
 Algorithm used: Classification, K-NearestNeighbors
 Accuracy : `0.7472527`
